79f9e5a07365160064009a13e467e1bfe4b4fc10
MJ Johnson <mjohnson4685@gmail.com>
2020-09-12 18:26:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,6 +25,8 @@ def assign_rooms(names)
 end
 
 def printer(attendees)
-   puts batch_badge_creator(name)
+   batch_badge_creator(attendees).each do |badge|
+     puts badge
+     
    puts assign_rooms(name)
 end
