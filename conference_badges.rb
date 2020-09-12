b15fa6bc0f45546f08b5aecabcca7da09cd714df
MJ Johnson <mjohnson4685@gmail.com>
2020-09-12 17:34:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name)
+
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+
+  badges = []
+
+  names.each  { |name| badges.push("Hello, my name is #{name}.")}
+
+  end
+
+  return badges
+
+end
+
+def assign_rooms(names)
+
+  rooms = []
+
+  rooms.each_with_index do |name, index|
+
+    door = index+1
+
+    rooms.push("Hello, #{name}! You'll be assigned to room #{door}!")
+
+  end
+
+  return rooms
+
+end
